d50d8ebb635c36a037bf4206edb402a282dbc74b
Jefferson <76830922+Jefferson1804@users.noreply.github.com>
2023-03-19 16:14:07 -0500

    initial iav settup

---
+++ src/app/modules/angular-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const materialModules = [
+    CommonModule
+]
+
+@NgModule({
+    imports: materialModules,
+    exports: materialModules,
+    declarations: []
+  })
+  export class AngularMaterialModule { }
