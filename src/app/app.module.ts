804ac57d7235a9d0268d442e3a4c74dfe2d70566
Jefferson <76830922+Jefferson1804@users.noreply.github.com>
2023-03-19 19:17:32 -0500

    created toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularMaterialModule } from './modules/angular-material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/angular-material.module.ts
+++ src/app/modules/angular-material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 const materialModules = [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
 ]
 
 @NgModule({
